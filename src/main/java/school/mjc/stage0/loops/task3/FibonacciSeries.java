1d7f0fa246fc68d943630f5a0cf0d56ab539b620
Artak1999 <dashyan.1999@mail.ru>
2022-07-06 19:03:46 +0500

    No questions.

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int n = 0, m = 1;
+        for (int i = 1; i <= lastFibonacci; i++) {
+            System.out.println(n);
+            int sum = n + m;
+            n = m;
+            m = sum;
+        }
     }
 }
