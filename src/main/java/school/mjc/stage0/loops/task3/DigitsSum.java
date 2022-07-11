17d572ca8659edfe0bdc67ed184db1b6f20200c5
Artak1999 <dashyan.1999@mail.ru>
2022-07-11 23:33:08 +0500

    No questions.

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,11 +2,9 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-        int sum = 0;
-        for (int i = t; i > 0; i--) {
-            int a = t % 10;
-            sum += a;
-            t /= 10;
+        int sum;
+        for(sum=0; t!=0; t=t/10)  {
+            sum = sum + t % 10;
         }
         System.out.println(sum);
     }
