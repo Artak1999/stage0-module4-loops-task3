040ba4eff4db6e1f640483b49a129bec17964ff8
Artak1999 <dashyan.1999@mail.ru>
2022-07-11 23:22:48 +0500

    No questions.

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
-        for (int i = 65; i < 91; i++) {
+        for (int i = 97; i < 123; i++) {
             System.out.println((char)i);
         }
     }
